f510de8ae5d6604788398a18588e8cd98208e561
AmiltonOsousa <119018582+AmiltonOsousa@users.noreply.github.com>
2023-10-24 16:44:39 -0300

    Projeto

--- PI/PI.cpp
+++ PI/PI.cpp
@@ -1,20 +1,50 @@
-// PI.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
+#include <stdio.h>
 #include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_native_dialog.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+//variáveis globais
+const int SCREEN_W = 1920;
+const int SCREEN_H = 1080;
+
+
+int main() {
+
+	// Variável representando a janela principal
+	ALLEGRO_DISPLAY* display = NULL;
+
+	// Variável representando a imagem
+	ALLEGRO_BITMAP* image = NULL;
+
+	// Inicializamos a biblioteca e apresentar erro caso o al init não retorne 0
+	if (!al_init()) {
+		printf("Falha na inicialização");
+		return -1;
+	}
+
+	// Inicializar add-on para usar imagem
+	al_init_image_addon();
+
+	//---------------------------TELA---------------------------\\
+
+	// Configura a janela
+	display = al_create_display(SCREEN_W, SCREEN_H);
+
+	// Carrega a imagem e desenha
+	image = al_load_bitmap("tela_principal.png");
+	al_draw_bitmap(image, 0, 0, 0);
+
+
+	// Atualiza a tela quando tiver algo para mostrar
+	al_flip_display();
+
+	// Segura a execução por 10 segundos
+	al_rest(10.0);
+
+	// Finaliza a janela
+	al_destroy_display(display);
+
+	return 0;
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
